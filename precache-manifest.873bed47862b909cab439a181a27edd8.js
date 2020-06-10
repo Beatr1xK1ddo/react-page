e22a784db05b7ef69bf39d540f9f34b1b87c0354
Alexa <kan_off@mail.ru>
2020-06-10 14:31:53 +0300

    Updates

---
+++ precache-manifest.873bed47862b909cab439a181a27edd8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce37833811db16d92a4f19a487e67e6f",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "9b3cc01be3efec7cef45",
+    "url": "/react-page/static/css/main.a1edee9f.chunk.css"
+  },
+  {
+    "revision": "f2c749122931cc6bd1d3",
+    "url": "/react-page/static/js/2.f2f1162c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.f2f1162c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3cc01be3efec7cef45",
+    "url": "/react-page/static/js/main.3cbdaa41.chunk.js"
+  },
+  {
+    "revision": "55df7b239c5dc3b4e82e",
+    "url": "/react-page/static/js/runtime-main.16e7ab51.js"
+  },
+  {
+    "revision": "8fa4edfee8103ac2b9076a1b8b4e9770",
+    "url": "/react-page/static/media/bg.8fa4edfe.jpg"
+  },
+  {
+    "revision": "81cbd86424c84beaba1c6be2f889bce6",
+    "url": "/react-page/static/media/envelope-regular.81cbd864.svg"
+  },
+  {
+    "revision": "7dbee6332fc070e19a30db384264a387",
+    "url": "/react-page/static/media/facebook-profile.7dbee633.svg"
+  },
+  {
+    "revision": "fa5228755353c54581fc56682a025f7c",
+    "url": "/react-page/static/media/instagram-brands.fa522875.svg"
+  },
+  {
+    "revision": "94d81ac603f816dc4ebf5353e907e6d6",
+    "url": "/react-page/static/media/instagram-profile.94d81ac6.svg"
+  },
+  {
+    "revision": "05f302b129fbd9f22db56952b603bee1",
+    "url": "/react-page/static/media/item-1.05f302b1.jpg"
+  },
+  {
+    "revision": "cea6f6e1909df1ff08d63947e9655615",
+    "url": "/react-page/static/media/item-post.cea6f6e1.jpg"
+  },
+  {
+    "revision": "d803698b02ae9b7bd349c02e151a8535",
+    "url": "/react-page/static/media/location-icon.d803698b.svg"
+  },
+  {
+    "revision": "d0b0f26bca64997394a55b0f38033b8c",
+    "url": "/react-page/static/media/login-bg-img.d0b0f26b.jpg"
+  },
+  {
+    "revision": "b897e3380054b6044f3daecd37c1ba27",
+    "url": "/react-page/static/media/profile-bg.b897e338.jpg"
+  },
+  {
+    "revision": "899cedbc00c239f172e748775ca514f2",
+    "url": "/react-page/static/media/profile-icon.899cedbc.svg"
+  },
+  {
+    "revision": "879a34848aaa038e129ccf4dad5ff303",
+    "url": "/react-page/static/media/rating-icon.879a3484.svg"
+  },
+  {
+    "revision": "16f915de84e27f1265b60aa1cd3a6d88",
+    "url": "/react-page/static/media/slide-1.16f915de.jpg"
+  },
+  {
+    "revision": "c73d0888f04c0d3c3f4f189773546eb2",
+    "url": "/react-page/static/media/telegram-plane-brands.c73d0888.svg"
+  },
+  {
+    "revision": "7f8911cde0e180e463a9ec9afcc76057",
+    "url": "/react-page/static/media/vk-brands.7f8911cd.svg"
+  },
+  {
+    "revision": "5838a8b257a199b45f1f0f8d265a7d65",
+    "url": "/react-page/static/media/vk-profile.5838a8b2.svg"
+  },
+  {
+    "revision": "e50e87fe68a051c7a16cc27b3e1b2d91",
+    "url": "/react-page/static/media/youtube-brands.e50e87fe.svg"
+  }
+]);
